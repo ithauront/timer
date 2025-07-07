dd2d03c44b2088d3459f25eac76a0ddc06265dd8
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2025-07-07 13:15:28 +0200

    config[root]: prepare for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-// Emular __dirname
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
+  base: '/timer/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
